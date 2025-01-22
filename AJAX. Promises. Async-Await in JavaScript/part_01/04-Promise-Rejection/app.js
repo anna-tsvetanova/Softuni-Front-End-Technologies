1f429ef2c77d9d1ef8b2cc139a62dc57cccea7b8
Anna Tsvetanova <159261341+anna-tsvetanova@users.noreply.github.com>
2025-01-22 18:36:44 +0200

    Update app.js

--- AJAX. Promises. Async-Await in JavaScript/part_01/04-Promise-Rejection/app.js
+++ AJAX. Promises. Async-Await in JavaScript/part_01/04-Promise-Rejection/app.js
@@ -1,7 +1,7 @@
 function promiseRejection() {
     new Promise((resolve, reject) => {
         setTimeout (() => {
-            reject("Smth went wrong!");
+            reject(new Error("Smth went wrong!"));
         }, 1000);
     })
     .catch((error) => {console.log(error)});
