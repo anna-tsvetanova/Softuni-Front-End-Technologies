77b6263e60967edcd22883701c00179b8ab3774f
Anna Tsvetanova <159261341+anna-tsvetanova@users.noreply.github.com>
2025-01-22 13:27:24 +0200

    Update app.js

--- AJAX. Promises. Async-Await in JavaScript/part_01/02-Chained-Promises/app.js
+++ AJAX. Promises. Async-Await in JavaScript/part_01/02-Chained-Promises/app.js
@@ -1,1 +1,18 @@
+function chainedPromises() {
+    console.log("Start");
 
+    setTimeout(() => {
+        console.log("1");
+        setTimeout(() => {
+            console.log("2");
+            setTimeout(() => {
+                console.log("3");
+            }
+            , 1000);
+        }
+        , 1000);
+    }
+    , 1000);
+}
+
+chainedPromises();
