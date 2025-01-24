307474dad4783e8125ac9ff2d55eb2c677e3549f
Anna Tsvetanova <159261341+anna-tsvetanova@users.noreply.github.com>
2025-01-24 22:35:25 +0200

    Update app.js

--- AJAX. Promises. Async-Await in JavaScript/part_01/09-Async-Function-with-Error-Handling/app.js
+++ AJAX. Promises. Async-Await in JavaScript/part_01/09-Async-Function-with-Error-Handling/app.js
@@ -1,1 +1,15 @@
+async function promiseRejectionAsync() {
+   try{
+      await new Promise((resolve, reject) => {
+         setTimeout(() => {
+            reject(new Error("oops!"))
+         } , 1000);
+      })
+   }
+   catch (error)
+   {
+      console.error(error);
+   }
+}
 
+promiseRejectionAsync()
