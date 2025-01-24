be4318c102c929336689ac98d1025676f7e76695
Anna Tsvetanova <159261341+anna-tsvetanova@users.noreply.github.com>
2025-01-24 12:29:49 +0200

    Update app.js

--- AJAX. Promises. Async-Await in JavaScript/part_01/08-Async-Function-with-Await/app.js
+++ AJAX. Promises. Async-Await in JavaScript/part_01/08-Async-Function-with-Await/app.js
@@ -1,1 +1,12 @@
+async function simplePromiseAsync() {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    console.log("Async/Await is awesome!");
+}
 
+simplePromiseAsync()
+
+function consoleLog()
+{
+    console.log("Im Here");
+}
+consoleLog()
