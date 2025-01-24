51f119ead1ec6b5bc93b319c6d7de74f79c62b20
Anna Tsvetanova <159261341+anna-tsvetanova@users.noreply.github.com>
2025-01-24 23:05:43 +0200

    Update app.js

--- AJAX. Promises. Async-Await in JavaScript/part_01/10-Chained-Promises-with-Async-Await/app.js
+++ AJAX. Promises. Async-Await in JavaScript/part_01/10-Chained-Promises-with-Async-Await/app.js
@@ -1,1 +1,20 @@
+async function chainedPromisesAsync() {
+    const p1 = await new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("First");
+        }, 1000)
+    });
+    const p2 = await new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Second");
+        }, 2000)
+    })
+    const p3 = await new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Third");
+        }, 3000)
+    });
+    console.log(p1, p2, p3);
+}
 
+chainedPromisesAsync()
