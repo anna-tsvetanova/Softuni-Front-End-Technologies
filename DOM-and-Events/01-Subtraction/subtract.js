c60f7582ce419034e65d53ce1356cbb11f30c2a1
Anna Tsvetanova <159261341+anna-tsvetanova@users.noreply.github.com>
2025-02-03 18:02:48 +0200

    Update subtract.js

--- DOM-and-Events/01-Subtraction/subtract.js
+++ DOM-and-Events/01-Subtraction/subtract.js
@@ -1,1 +1,9 @@
+function subtract() {
+    let num1 = Number(document.getElementById('firstNumber').value);
+    let num2 = Number(document.getElementById('secondNumber').value);
 
+    let sum = num1 - num2;
+
+    let resultDiv = document.getElementById('result');
+    resultDiv.textContent = sum;
+}
