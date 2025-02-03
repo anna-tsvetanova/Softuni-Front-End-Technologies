7b8cb242b9e794a5da913eef5955419f7b92d6aa
Anna Tsvetanova <159261341+anna-tsvetanova@users.noreply.github.com>
2025-02-03 19:03:20 +0200

    Update solution.js

--- DOM-and-Events/02-Pascal-or-Camel-Case/solution.js
+++ DOM-and-Events/02-Pascal-or-Camel-Case/solution.js
@@ -1,1 +1,25 @@
+function solve() {
+  let input = document.getElementById('text').value.toLowerCase();
+  let currentCase = document.getElementById('naming-convention').value;
+  let resultSpan = document.getElementById('result');
 
+  let splited = input.split(' ');
+  let resultString = '';
+
+  if (currentCase == 'Camel Case') {
+    resultString += splited[0];
+    for (let i = 1; i < splited.length; i++) {
+      resultString += splited[i][0].toUpperCase() + splited[i].slice(1, splited[i].length);
+      resultSpan.textContent = resultString;
+    }
+
+  } else if (currentCase == 'Pascal Case') {
+    for (let i = 0; i < splited.length; i++) {
+      resultString += splited[i][0].toUpperCase() + splited[i].slice(1, splited[i].length);
+      resultSpan.textContent = resultString;
+    }
+  } else {
+    resultSpan.textContent = 'Error!';
+  }
+
+}
