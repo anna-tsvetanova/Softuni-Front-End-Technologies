2848899a84bc136c1a580907913a1f410b3f438b
Anna Tsvetanova <159261341+anna-tsvetanova@users.noreply.github.com>
2025-02-04 12:17:49 +0200

    Update script.js

--- DOM-and-Events/05-Sections/script.js
+++ DOM-and-Events/05-Sections/script.js
@@ -1,1 +1,19 @@
+function create(words) {
+   let content = document.getElementById('content');
 
+   for (const word of words) {
+      let div = document.createElement('div');
+      let para = document.createElement('p');
+      para.textContent = word;
+      para.style.display = 'none'
+      
+      div.appendChild(para);
+      content.appendChild(div);
+
+      div.addEventListener('click', reveal)
+ 
+      function reveal(event) {
+         event.currentTarget.children[0].style.display = 'block';
+      }
+   }
+}
