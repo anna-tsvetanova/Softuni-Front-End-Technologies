bd2ad3f3abcb391c00fb3f651e9aa9cbf53162e9
Anna Tsvetanova <159261341+anna-tsvetanova@users.noreply.github.com>
2025-02-04 10:52:05 +0200

    Update accordion.js

--- DOM-and-Events/03-Accordion/accordion.js
+++ DOM-and-Events/03-Accordion/accordion.js
@@ -1,1 +1,15 @@
+function toggle() {
+    let button = document.querySelector(".button");
+    let hiddenText = document.getElementById("extra");
 
+    if(button.textContent == "More")
+    {
+        hiddenText.style.display = "block";
+        button.textContent = "Less"
+    }
+    else if(button.textContent == "Less")
+    {
+        hiddenText.style.display = "none"
+        button.textContent = "More"
+    }
+}
