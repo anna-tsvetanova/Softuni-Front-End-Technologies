b60fea44e3050a1f7ef8a4704dc01293ffd946a2
Anna Tsvetanova <159261341+anna-tsvetanova@users.noreply.github.com>
2025-02-07 15:32:43 +0200

    Update ui.test.js

--- Playwright/All_Books_page/ui.test.js
+++ Playwright/All_Books_page/ui.test.js
@@ -1,1 +1,17 @@
+const { test, expect } = require ('@playwright/test');
 
+test('Verify "All Books" link is visible', async ({page}) => {
+    
+    //Act
+    await page.goto('http://localhost:3000');
+
+    await page.waitForSelector('nav.navbar');
+
+    const allBooksLink = await page.locator('a[href="/catalog"]');
+
+    const isAllBooksLinkVisible = await allBooksLink.isVisible();
+
+    // Assert
+    expect(isAllBooksLinkVisible).toBe(true)
+
+})
