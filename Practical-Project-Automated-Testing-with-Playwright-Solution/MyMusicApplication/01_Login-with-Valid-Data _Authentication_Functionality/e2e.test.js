566d386b974ca1f9217413936d5ef343d4f5cbc7
Anna Tsvetanova <159261341+anna-tsvetanova@users.noreply.github.com>
2025-02-13 16:27:41 +0200

    Update e2e.test.js

--- Practical-Project-Automated-Testing-with-Playwright-Solution/MyMusicApplication/01_Login-with-Valid-Data _Authentication_Functionality/e2e.test.js
+++ Practical-Project-Automated-Testing-with-Playwright-Solution/MyMusicApplication/01_Login-with-Valid-Data _Authentication_Functionality/e2e.test.js
@@ -1,1 +1,15 @@
+test("login with valid data", async () => {
+            //Arrange
+            await page.goto(host);
+            await page.click("//a[@href='/login']");
+            await page.waitForSelector("//form");
 
+            //Act
+            await page.fill("//input[@name='email']", user.email);
+            await page.fill("//input[@id='password']", user.password);
+            await page.click("//button[text()='Login']");
+
+            //Asserts
+            await expect(page.locator("//a[text()='Logout']")).toBeVisible();
+            expect(page.url()).toBe(host + "/");
+        })
