f8f9d5e5289fcdf469c0cc94716d2c2cb61e169b
Anna Tsvetanova <159261341+anna-tsvetanova@users.noreply.github.com>
2025-02-13 16:28:59 +0200

    Update e2e.test.js

--- Practical-Project-Automated-Testing-with-Playwright-Solution/MyMusicApplication/01_Logout-from-the-Application_Authentication_Functionality/e2e.test.js
+++ Practical-Project-Automated-Testing-with-Playwright-Solution/MyMusicApplication/01_Logout-from-the-Application_Authentication_Functionality/e2e.test.js
@@ -1,1 +1,17 @@
+test("logout from the application", async ()=> {
+            //Arrange
+            await page.goto(host);
+            await page.click("//a[@href='/login']");
+            await page.waitForSelector("//form");
 
+            await page.fill("//input[@name='email']", user.email);
+            await page.fill("//input[@id='password']", user.password);
+            await page.click("//button[text()='Login']");
+
+            //Act
+            await page.click("//a[text()='Logout']");
+
+            //asserts
+            await expect(page.locator("//a[text()='Login']")).toBeVisible();
+            expect(page.url()).toBe(host + "/")
+        })
